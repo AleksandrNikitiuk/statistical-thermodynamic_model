b7dcf6d69544c0f63c87f4c3c5169affacf31c9a
Aleksandr S. Nikitiuk <31177834+AleksandrNikitiuk@users.noreply.github.com>
2022-11-18 13:29:42 +0500

    Implement loading-dwell experiment

--- simulate_cell_mechanical_response.m
+++ simulate_cell_mechanical_response.m
@@ -1,44 +1,55 @@
 time_step = .013;
 time = (time_step:time_step:10)';
+time_loading = time(time <= 1);
+time_dwell = time(time > 1);
 
 sigma_0 = 0.;
 initial_conditions = sigma_0;
 
-E1 = 20;
-E2 = 1;
+E1 = 300;
+E2 = 100;
 Lo = 1;
-nu = 2;
+nu = 100;
 
-eps = .05;
+eps = .005;
 deps_dt = eps * time_step;
+deps_dt_dwell = 0;
 
-theta = [1 2 3];
+theta = [.2];
 line_style = {'-','--',':'};
 
 for i = 1:length(theta)
-  [~,Sigma] = ode15s(@(t,sigma) get_rhs_expr(t,sigma,eps,deps_dt,E1,E2,Lo,nu,theta(i)),time,initial_conditions);
+  % loading
+  [~,Sigma] = ode15s(@(t,sigma) get_rhs_expr(t,sigma,eps,deps_dt,E1,E2,Lo,nu,theta(i)),time_loading,initial_conditions);
+  
+  % dwell
+  initial_conditions = Sigma(end);
+  [~,Sigma_dwell] = ode15s(@(t,sigma) get_rhs_expr(t,sigma,eps,deps_dt_dwell,E1,E2,Lo,nu,theta(i)),time_dwell,initial_conditions);
+  Sigma = [Sigma; Sigma_dwell];
 
-  figure(1);hold on
-  plot((eps*time),Sigma,'k','LineWidth',2,'LineStyle',line_style{i})
-  xlabel(['{\it' char(949) '}']);
-  ylabel('\sigma');
-  set_figure;
-
-  figure(2);hold on
-  plot(time,Sigma ./ (eps*time),'k','LineWidth',2,'LineStyle',line_style{i})
-  xlabel('time');
-  ylabel('E');
-  set_figure;
+  eps_time = [eps*time_loading; eps * ones(length(time_dwell),1)];
+  
+%   figure(1);hold on
+%   plot(eps_time,Sigma,'k','LineWidth',2,'LineStyle',line_style{i})
+%   xlabel(['{\it' char(949) '}']);
+%   ylabel('\sigma');
+%   set_figure;
+% 
+%   figure(2);hold on
+%   plot(time,Sigma ./ eps_time,'k','LineWidth',2,'LineStyle',line_style{i})
+%   xlabel('time');
+%   ylabel('E');
+%   set_figure;
 
   figure(3);hold on
-  plot(time,(Sigma ./ (eps*time)) .* (eps*time).^(3/2),'k','LineWidth',2,'LineStyle',line_style{i})
+  plot(time,(Sigma ./ eps_time) .* eps_time.^(3/2),'k','LineWidth',2,'LineStyle',line_style{i})
   xlabel('time');
   ylabel('F');
   set_figure;
 end
-for i = 1:3
-  figure(i);legend(compose('{\\it\\Theta}=%.2f',theta));
-end
+% for i = 1:3
+%   figure(i);legend(compose('{\\it\\Theta}=%.2f',theta));
+% end
 
 
 %%
@@ -115,6 +126,10 @@ end
 
 if nargin < 4
   deps_dt = 0.;
+end
+
+if deps_dt == 0
+  eps = eps / t;
 end
 
 rhs_expr = zeros(size(varibles));
